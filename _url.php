bd7f89262be1376f55e738f6d388518680ba4c8d
RRZGriffin <94623547+RRZGriffin@users.noreply.github.com>
2023-08-31 23:53:59 +0800

    Add files via upload

---
+++ _url.php
@@ -0,0 +1,6 @@
+<?php
+
+      $homeurl = "http://localhost/PHP-Codes/BSCS-3-Profiling-System/PHP_Style/";
+	  $subadminurl = "http://localhost/PHP_Codes/BSCS-3-Profiling-System/PHP_Style/p_sub_admin/";
+
+      $homedir = "C:"."\x"."ampp\htdocs\PHP-Codes\BSCS-3-Profiling-System\PHP_Style";
